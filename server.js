59300f3a5fb1a8c1568b028d4640eb7a4266b82b
usmankhanefan <usmankhan12719@gmail.com>
2026-02-28 09:02:51 +0600

    server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.post('/api/get-trial', (req, res) => {
 
   // নতুন trial তৈরি করি
   const trialKey = generateTrialKey();
-  const expiry = new Date(Date.now() + 1 * 60 * 60 * 1000).toISOString(); // ১ ঘন্টা
+  const expiry = new Date(Date.now() + 2 * 60 * 60 * 1000).toISOString(); // ২ ঘন্টা
 
   trials.used_pcs[hashedPc] = {
     key: trialKey,
